56425658752a01f7a67e7ee649de8de4df2e8a4b
Fighter077 <86990216+Fighter077@users.noreply.github.com>
2025-02-02 12:18:21 +0100

    Making scripts executable4

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/chessButBetter/backend
-nohup java -jar target/chessButBetter-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /home/ec2-user/chessButBetter/logs/backend.log 2>&1 &
+nohup java -jar target/chessButBetter-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /home/ec2-user/chessButBetter/backend/logs/logs.log 2>&1 &
