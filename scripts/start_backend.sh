a8aae6deae3393798732cd809214741dd966d7f0
Fighter077 <86990216+Fighter077@users.noreply.github.com>
2025-02-02 10:53:17 +0100

    hooks and envs

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/chessButBetter/backend
-nohup java -jar target/chessButBetter-0.0.1-SNAPSHOT.jar > /home/ec2-user/chessButBetter/logs/backend.log 2>&1 &
+nohup java -jar target/chessButBetter-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /home/ec2-user/chessButBetter/logs/backend.log 2>&1 &
--- scripts/start_frontend.sh
+++ scripts/start_frontend.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/chessButBetter/frontend
-nohup npx http-server dist -p 80 > /home/ec2-user/chessButBetter/logs/frontend.log 2>&1 &
+sudo systemctl restart nginx
