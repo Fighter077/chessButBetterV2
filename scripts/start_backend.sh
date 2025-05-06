5fada802cccf58122f07fbf7ddd7dfc0ad3012c2
Fighter077 <86990216+Fighter077@users.noreply.github.com>
2025-05-06 19:12:31 +0200

    enabled ssl and updated script

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -6,4 +6,14 @@ echo "Running $(basename "$0")..." >> "$LOG_FILE"
 echo "Starting backend..." >> "$LOG_FILE"
 
 cd /home/ec2-user/chessButBetter/backend
+
+PID=$(pgrep -f 'chessButBetter-0.0.1-SNAPSHOT.jar' || true)
+
+if [ -n "$PID" ]; then
+  echo "Killing existing backend server (PID: $PID)..."
+  kill "$PID"
+  sleep 2
+fi
+
+
 nohup java -jar target/chessButBetter-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /home/ec2-user/chessButBetter/backend/logs/logs.log 2>&1 &
