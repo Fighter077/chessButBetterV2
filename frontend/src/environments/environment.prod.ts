d2f367733ddc5ae3e462d04ca4b5e733e1dfbb49
Fighter077 <86990216+Fighter077@users.noreply.github.com>
2025-05-06 19:36:25 +0200

    fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     'userApiUrl': 'https://www.chessbutbetter.com:5010/api',
-    'backendUrl': 'http://www.chessbutbetter.com:5010/api'
+    'backendUrl': 'https://www.chessbutbetter.com:5010/api'
   };
