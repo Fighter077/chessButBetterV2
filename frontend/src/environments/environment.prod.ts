5a40f1d3a0c717e56bfee341815a2117f6b06581
Fighter077 <86990216+Fighter077@users.noreply.github.com>
2025-07-21 00:03:52 +0200

    fixed hmr prod failure

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
+    hmr: false,
     'userApiUrl': 'https://www.chessbutbetter.com/api',
     'backendUrl': 'https://www.chessbutbetter.com/api',
     'only-allowed-to-play-once': true
